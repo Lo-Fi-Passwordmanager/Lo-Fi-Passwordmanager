465cc58804bf57f4caceacfa2d2ebb055faccf92
SwaggiSpaghetti <jesko.kissner@mailbox.org>
2026-01-12 18:26:05 +0100

    Fixed settings allignement and moved CSS File

--- src/Components/Views/SettingsView.tsx
+++ src/Components/Views/SettingsView.tsx
@@ -1,64 +1,60 @@
 import {useSettingsViewModel} from "../ViewModels/SettingsViewModel.ts";
+import {checkboxRowStyle, settingsContainer} from "../../CSS.ts";
 
 const SettingsView: React.FC = () => {
     const viewmodel = useSettingsViewModel();
 
-
     return (
         <div>
-            <h1> Settings </h1>
-            <div style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-            }}>
+            <h1 style={{ fontSize: "2em", marginBottom: "20px" }}>Settings</h1>
 
+            <div style={settingsContainer}>
                 <div>
-                    <label>
+                    <label style={checkboxRowStyle}>
                         <input
                             type="checkbox"
                             checked={viewmodel.darkMode}
                             onChange={viewmodel.toggleDarkMode}
                         />
+                        Darkmode
                     </label>
-                    <p>Darkmode</p>
                 </div>
 
                 <div>
-                    <label>
+                    <label style={checkboxRowStyle}>
                         <input
                             type="checkbox"
                             checked={viewmodel.synchronisation}
                             onChange={viewmodel.toggleSynchronisation}
                         />
+                        Synchronisation
                     </label>
-                    <p>Synchronisation</p>
                 </div>
 
                 <div>
-                    <label>
+                    <label style={checkboxRowStyle}>
                         <input
                             type="checkbox"
                             checked={viewmodel.autoConclictRes}
                             onChange={viewmodel.toggleAutoConclictRes}
                         />
+                        Konfliktauflösung
                     </label>
-                    <p>Konfliktauflösung</p>
                 </div>
 
                 <div>
-                    <label>
+                    <label style={checkboxRowStyle}>
                         <input
                             type="checkbox"
                             checked={viewmodel.timeOutActive}
                             onChange={viewmodel.toggleTimeOutActive}
                         />
+                        Sperren der App bei Inaktivität
                     </label>
-                    <p>Sperren der App bei Inaktivität</p>
                 </div>
             </div>
         </div>
-);
+    );
 }
 
 export default SettingsView;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import LoginView from './Components/Views/LoginView.tsx'
+import App from './App.tsx'
+import SettingsView from "./Components/Views/SettingsView.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LoginView />
+    <SettingsView />
   </StrictMode>,
 )
