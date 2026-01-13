68aeb0eb54020981405dd2c511f9155e0fbb47fe
SwaggiSpaghetti <jesko.kissner@mailbox.org>
2026-01-13 14:20:37 +0100

    added functionality for toggeling modes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import SettingsView from "./Components/Views/SettingsView.tsx";
 import LoginView from './Components/Views/LoginView.tsx'
 import "./styles.css"
+import {Settings} from "./Model/Settings.ts";
+
+const theme = Settings.getSettings().getDarkMode() ? "dark" : "light";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <div data-theme={"dark"}>
-        <LoginView />
+      <div data-theme={theme}>
+          <LoginView />
+          <SettingsView />
       </div>
   </StrictMode>,
 )
