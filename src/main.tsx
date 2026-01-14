d41b7fa594929f3865110d18ec77b40e9d096ad1
SwaggiSpaghetti <jesko.kissner@mailbox.org>
2026-01-14 13:20:19 +0100

    [fix] auto update darkmode

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,10 @@ import './index.css'
 import SettingsView from "./Components/Views/SettingsView.tsx";
 import LoginView from './Components/Views/LoginView.tsx'
 import "./styles.css"
-import {Settings} from "./Model/Settings.ts";
-
-const theme = Settings.getSettings().getDarkMode() ? "dark" : "light";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <div data-theme={theme}>
           <LoginView />
           <SettingsView />
-      </div>
   </StrictMode>,
 )
