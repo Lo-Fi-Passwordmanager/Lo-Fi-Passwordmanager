7e81a27da10f2b765e08c7f05a42585359302fc8
SwaggiSpaghetti <jesko.kissner@mailbox.org>
2026-01-20 21:18:18 +0100

    [feat] made link clickable

--- src/Components/Views/EntryView.tsx
+++ src/Components/Views/EntryView.tsx
@@ -16,7 +16,13 @@ const EntryView: React.FC<{ item: Item}> = ({item}) => {
                             <span>Titel:</span>        <span>{entry.title}</span>
                             <span>Benutzername:</span> <span>{entry.username}</span>
                             <span>Passwort:</span>     <span>{entry.password}</span>
-                            <span>URL:</span>          <span>{entry.url}</span>
+                        {/* adds https://www. to the start of the link*/}
+                            <span>URL:</span>          <a href={entry.url.includes("www.") ?
+                                                                (entry.url.startsWith("http")?entry.url:("https://"+entry.url))
+                                                                :("https://www."+entry.url)}
+                                                          target="_blank" rel="noopener noreferrer"
+                                                            style={{textDecoration: "underline", color: "inherit"}}>
+                        {entry.url}</a>
                             <span>Notiz:</span>        <span>{entry.note}</span>
                     </div>
                 );
