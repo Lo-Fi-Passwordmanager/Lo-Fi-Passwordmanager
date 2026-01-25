f412b000332c792c847a7554282b40be6565e28e
Chalg0 <sprudelwasser100@gmail.com>
2026-01-25 23:43:28 +0100

    continued UseLoginViewModel.test.ts

--- tests/Components/ViewModels/UseLoginViewModel.test.ts
+++ tests/Components/ViewModels/UseLoginViewModel.test.ts
@@ -1,11 +1,10 @@
 import {afterEach, beforeEach, describe, expect, it, vi} from "vitest";
-import {act, renderHook} from "@testing-library/react";
+import {act, renderHook, waitFor} from "@testing-library/react";
 import {useLoginViewModel} from "../../../src/Components/ViewModels/UseLoginViewModel";
 import {Repo} from "@automerge/react";
 import {SecurityProvider} from "../../../src/Utility/Security/SecurityProvider";
-import {loadAllDatabases} from "../../../src/Utility/Storage";
 
-describe('UseLoginViewModel',()=> {
+describe('UseLoginViewModel', () => {
     let repo;
     const setLoggedIn = vi.fn();
     const setAutomergeFacade = vi.fn();
@@ -14,38 +13,115 @@ describe('UseLoginViewModel',()=> {
     beforeEach(() => {
         repo = new Repo();
         secProv = new SecurityProvider();
-        let vali = secProv.getNewValidation("password", secProv.getNewSalt());
+        secProv.getNewValidation("password", secProv.getNewSalt());
     })
 
     afterEach(() => {
 
     })
 
+
     it("should be able to create a new Database", async () => {
-        const { result } = renderHook(() =>
+        const {result} = renderHook(() =>
             useLoginViewModel(repo, setLoggedIn, setAutomergeFacade, secProv, setOpenedDbName));
+        act(() => {
+            result.current.createDatabase("name", "password");
+        })
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(1);
+            expect(setLoggedIn).toHaveBeenCalled();
+        });
+    })
+
+    it('should not create a new database if the name already exists', async () => {
+        const {result} = renderHook(() =>
+            useLoginViewModel(repo, setLoggedIn, setAutomergeFacade, secProv, setOpenedDbName));
+        act(() => {
+            result.current.createDatabase("name", "password");
+        })
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(1);
+        });
+        act(() => {
+            result.current.createDatabase("name", "Password");
+        })
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(1);
+        });
+    });
+
+    it('should be able to delete a database', async () => {
+        const {result} = renderHook(() =>
+            useLoginViewModel(repo, setLoggedIn, setAutomergeFacade, secProv, setOpenedDbName));
+        act(() => {
+            result.current.createDatabase("name", "password");
+        })
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(1);
+        });
+        act(() => {
+            result.current.deleteDatabase("name");
+        })
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(0);
+        });
+    })
+
+    it('should an error if a database is attempted to be opened but there is not database given', async () => {
+        const {result} = renderHook(() =>
+            useLoginViewModel(repo, setLoggedIn, setAutomergeFacade, secProv, setOpenedDbName));
+        await expect(result.current.tryOpenDatabase("password")).rejects.toThrow("No database selected");
+    })
+
+    it('should be able to import a database from a url', async () => {
+        const {result} = renderHook(() =>
+            useLoginViewModel(repo, setLoggedIn, setAutomergeFacade, secProv, setOpenedDbName));
+        let database;
         act(()=> {
             result.current.createDatabase("name", "password");
         })
-        act(() => {
-            console.log(result.current.databases);
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(1);
+        });
+        act(()=> {
+            database = result.current.databases.get("name");
         })
-        const databases = loadAllDatabases();
-        console.log(databases);
-        //expect(result.current.databases.size).toBe(1);
+        act(()=> {
+            result.current.deleteDatabase("name");
+        })
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(0);
+        });
+        act(()=> {
+            result.current.importDatabaseFromURL("name", database);
+        })
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(1);
+        });
     })
-})
 
-function createMockRepo() {
-    return {
-        create: vi.fn().mockReturnValue({
-            url: "automerge:mock-url",
-        }),
-        find: vi.fn().mockResolvedValue({
-            doc: () => ({
-                salt: "salt123",
-                validation: "val123",
-            }),
-        }),
-    } as unknown as Repo
-}
+    it('should be able to reject a wrong import from a url', async ()=> {
+        const { result } = renderHook(() =>
+            useLoginViewModel(repo, setLoggedIn, setAutomergeFacade, secProv, setOpenedDbName));
+        act(() => {
+            result.current.createDatabase("name", "password");
+        });
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(1);
+        });
+        await waitFor(() => {
+            expect(result.current.databases.get("name")).toBeDefined();
+        });
+        const database= result.current.databases.get("name");
+
+        act(() => {
+            result.current.importDatabaseFromURL("name", database);
+        });
+
+        await waitFor(() => {
+            expect(result.current.databases.size).toBe(1);
+        });
+    });
+
+
+})
