824827ac5660b28da05bd97aeae703cdd5827239
Chalg0 <sprudelwasser100@gmail.com>
2026-02-04 00:42:08 +0100

    added a bit to DatabaseSettingsViewModel.test.ts

--- tests/Components/ViewModels/DatabaseSettingsViewModel.test.ts
+++ tests/Components/ViewModels/DatabaseSettingsViewModel.test.ts
@@ -3,12 +3,16 @@ import {act, renderHook} from "@testing-library/react";
 import {useDatabaseSettingsViewModel} from "../../../src/Components/ViewModels/DatabaseSettingsViewModel";
 
 describe('DatabaseSettingsViewModel', () => {
-    it('should', ()=> {
+    it('should be able to open the Settings correctly', ()=> {
         const {result} = renderHook(() => useDatabaseSettingsViewModel());
         expect(result.current.settingsOpen).toBe(false);
         act(() => {
             result.current.setSettingsOpen(true);
         })
         expect(result.current.settingsOpen).toBe(true);
+        act(() => {
+            result.current.setSettingsOpen(false);
+        })
+        expect(result.current.settingsOpen).toBe(false);
     })
 });
