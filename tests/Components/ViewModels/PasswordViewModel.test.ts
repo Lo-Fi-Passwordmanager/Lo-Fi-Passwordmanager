7e67f55a52d28494c6e10b25397cac58763607b4
Chalg0 <sprudelwasser100@gmail.com>
2026-02-03 13:43:42 +0100

    even more tests

--- tests/Components/ViewModels/PasswordViewModel.test.ts
+++ tests/Components/ViewModels/PasswordViewModel.test.ts
@@ -1,33 +1,65 @@
-import {afterEach, beforeEach, describe, it} from "vitest";
+import {afterEach, beforeEach, describe, it, expect, vi} from "vitest";
 import {AutomergeFacade} from "../../../src/Utility/AutomergeFacade";
 import {Repo} from "@automerge/react";
+import {renderHook, act} from "@testing-library/react";
+import {usePasswortViewModel} from "../../../src/Components/ViewModels/PasswordViewModel";
+import {Entry} from "../../../src/Model/Entry";
+import {Folder} from "../../../src/Model/Folder";
 
 describe('PasswordViewModel',() => {
     let automergeFacade;
     let repo;
+    let entry;
 
+    vi.mock("@automerge/react", async (importOriginal) => {
+        const actual = await importOriginal<typeof import("@automerge/react")>()
+
+        return {
+            ...actual,
+            useDocument: vi.fn(() => {
+                return [
+                    {
+                        salt: "aaaa",
+                        validation: "validation",
+                        items: [],
+                    },
+                    vi.fn(),
+                ]
+            }),
+        }
+    });
 
     beforeEach(()=> {
         repo = new Repo();
         automergeFacade = new AutomergeFacade(repo);
         automergeFacade.createDatabase("salt", "validation", "Database");
+        entry = new Entry("name", "id", new Date(), new Date(), "user", "pass", "url", "note");
     })
 
     afterEach(() => {
 
     })
 
-    it('should be able to toggle editable Password view',() => {
-        /*const { result } = renderHook(() => usePasswortViewModel(automergeFacade));
-        expect(result.current.getInEditablePasswordView()).toBe(false);
+    it('should be able toset the current item',() => {
+        const { result } = renderHook(() => usePasswortViewModel(automergeFacade));
         act(() => {
-            result.current.toggleEditablePasswordView();
+            result.current.setCurItem(entry);
         })
-        expect(result.current.getInEditablePasswordView()).toBe(true);
+        expect(result.current.getCurEntry()).toStrictEqual(entry);
+    });
+
+    it('should be able to toggle whether to hide the password', ()=> {
+        const { result } = renderHook(() => usePasswortViewModel(automergeFacade));
+        expect(result.current.hidePassword).toBe(true);
         act(() => {
-            result.current.toggleEditablePasswordView();
-        })
-        expect(result.current.getInEditablePasswordView()).toBe(false);*/
+            result.current.toggleHidePassword();
+        });
+        expect(result.current.hidePassword).toBe(false);
+        act(() => {
+            result.current.toggleHidePassword();
+        });
+        expect(result.current.hidePassword).toBe(true);
+
     })
 
 })
