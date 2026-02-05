f9981ba686019b05379237c7a84fd8322801156a
Fion G <uwing@student.kit.edu>
2026-02-05 13:42:28 +0100

    [fix] moved all logic back to folder menu since it wasnt working anymore

--- src/Components/Views/MenuViews/FolderMenu.tsx
+++ src/Components/Views/MenuViews/FolderMenu.tsx
@@ -1,14 +1,6 @@
-import React, {useEffect, useRef} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {HiMiniPlus} from "react-icons/hi2";
 import {HiPencil, HiTrash} from "react-icons/hi";
-import {useFolderMenuViewModel} from "../../ViewModels/Menu/FolderMenuViewModel.ts";
-
-interface Props {
-    onDelete: () => void;
-    onRename: () => void;
-    onAdd: () => void;
-    disabled: boolean;
-}
 
 /**
  * A 3-dot menu for folders which contains buttons to create an item, edit the title and delete the folder
@@ -18,57 +10,66 @@ interface Props {
  * @param onAdd method to add a new item to the folder
  * @param disabled whether the menu is disabled
  */
-const FolderMenu: React.FC<Props> = ({onDelete, onRename, onAdd, disabled}) => {
-
-    const viewModel = useFolderMenuViewModel();
+const FolderMenu: React.FC<{
+    onDelete: () => void;
+    onRename: () => void;
+    onAdd: () => void;
+    disabled: boolean;
+}> = ({onDelete, onRename, onAdd, disabled}) => {
+    const [isOpen, setIsOpen] = useState(false);
     const menuRef = useRef<HTMLDivElement>(null);
 
-    // close menu if clicked outside
     useEffect(() => {
         const handleClickOutside = () => {
-            if (menuRef.current && menuRef.current.contains(document.activeElement)) {
-                viewModel.setIsOpen(false);
+            if (menuRef.current && !menuRef.current.contains(document.activeElement)) {
+                setIsOpen(false);
             }
         };
         document.addEventListener("mousedown", handleClickOutside);
         return () => {
             document.removeEventListener("mousedown", handleClickOutside);
         };
-    }, [viewModel]);
-    
+    }, []);
+
+    // prevent click from dragging item
+    const toggleMenu = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className="action-menu-wrapper" ref={menuRef} onClick={(e) => e.stopPropagation()}>
 
-            <div className={`action-items ${viewModel.isOpen ? "open" : ""}`}>
+            <div className={`action-items ${isOpen ? "open" : ""}`}>
                 <button
                     className="listViewTitleHeader button"
-                    disabled={!viewModel.isOpen}
+                    disabled={!isOpen}
                     onClick={() => {
                         onAdd();
-                        viewModel.setIsOpen(false);
+                        setIsOpen(false);
                     }}>
                     <HiMiniPlus size={24}/>
                 </button>
                 <button
-                    disabled={!viewModel.isOpen}
+                    disabled={!isOpen}
                     className="listViewTitleHeader button"
                     onClick={() => {
                         onRename();
-                        viewModel.setIsOpen(false);
+                        setIsOpen(false);
                     }}>
                     <HiPencil size={24}/>
                 </button>
                 <button
                     className="listViewTitleHeader button"
-                    disabled={!viewModel.isOpen}
+                    disabled={!isOpen}
                     onClick={onDelete}>
                     <HiTrash size={24}/>
                 </button>
             </div>
 
             <button
-                className={`listViewTitleHeader button ${viewModel.isOpen ? "active" : ""}`}
-                onClick={viewModel.toggleMenu}
+                className={`listViewTitleHeader button ${isOpen ? "active" : ""}`}
+                onClick={toggleMenu}
                 disabled={disabled}
             >
                 ⋮
