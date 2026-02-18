f8d3fb940c6dab1a0e20a88477206111cbbeaa0c
SwaggiSpaghetti <jesko.kissner@mailbox.org>
2026-02-18 17:17:13 +0100

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,101 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist', 'node_modules']),
+
+    {
+        files: ['src/**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+
+            // TypeScript (type-aware)
+            ...tseslint.configs.recommendedTypeChecked,
+
+            // React
+            react.configs.flat.recommended,
+
+            // Hooks
+            reactHooks.configs.flat.recommended,
+
+            // Vite Fast Refresh
+            reactRefresh.configs.vite,
+        ],
+
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: globals.browser,
+            parserOptions: {
+                project: ['./tsconfig.json'],
+            },
+        },
+
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+
+        plugins: {
+            import: importPlugin,
+        },
+
+        rules: {
+            /* -----------------------------
+             * TypeScript
+             * --------------------------- */
+
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+            ],
+
+            '@typescript-eslint/consistent-type-imports': 'error',
+
+            '@typescript-eslint/no-misused-promises': 'error',
+
+            '@typescript-eslint/no-floating-promises': 'error',
+
+            '@typescript-eslint/explicit-function-return-type': 'off',
+
+            /* -----------------------------
+             * React
+             * --------------------------- */
+
+            'react/react-in-jsx-scope': 'off', // Not needed with new JSX transform
+
+            'react/prop-types': 'off', // Using TypeScript instead
+
+            'react/jsx-uses-react': 'off',
+
+            'react/jsx-boolean-value': ['warn', 'never'],
+
+            'react/self-closing-comp': 'warn',
+
+            /* -----------------------------
+             * Imports
+             * --------------------------- */
+
+            'import/order': [
+                'warn',
+                {
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        ['parent', 'sibling', 'index'],
+                    ],
+                    alphabetize: { order: 'asc', caseInsensitive: true },
+                    'newlines-between': 'always',
+                },
+            ],
+        },
     },
-  },
 ])
