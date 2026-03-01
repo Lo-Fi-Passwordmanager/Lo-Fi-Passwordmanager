007a78d0610d1a86d57c19017d0ce2eaca2a3171
RedCommander735 <40245965+RedCommander735@users.noreply.github.com>
2026-03-01 19:57:15 +0100

    [fix] delete item from array after searching for children and treat invalid parent ids as empty parent ids

--- src/Utility/AutomergeHelper.ts
+++ src/Utility/AutomergeHelper.ts
@@ -131,7 +131,14 @@ function buildPath(item: AutomergeItem, itemsById: Map<string, AutomergeItem>): 
         return [];
     }
 
-    return buildPath(itemsById.get(item.parentId)!, itemsById).concat(item.parentId);
+    const parent = itemsById.get(item.parentId);
+
+    if (parent === undefined) {
+        console.info("Found item with invalid parent id, treating as empty parent id.");
+        return [];
+    }
+
+    return buildPath(parent, itemsById).concat(item.parentId);
 }
 
 /**
@@ -205,15 +212,15 @@ export function deleteValue(d: AutomergeDoc, itemId: string, itemsById: Map<stri
 
     const index = d.items.indexOf(item);
 
-    d.items.splice(index, 1);
-
     if (isFolder(item)) {
-        for (const item of itemsById.values()) {
+        for (const item of d.items) {
             if (item.parentId === itemId) {
                 deleteValue(d, getObjectId(item)!, itemsById);
             }
         }
     }
+
+    d.items.splice(index, 1);
 }
 
 export function updateValue(d: AutomergeDoc, itemId: string, itemsById: Map<string, AutomergeItem>, attribute: Attribute, newValue: string | Date) {
