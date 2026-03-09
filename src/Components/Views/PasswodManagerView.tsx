d9f279689a3ce09191fd0b7f39c5c323bb5ad60b
SwaggiSpaghetti <jesko.kissner@mailbox.org>
2026-03-09 13:17:50 +0100

    [fix] made logo clickable to home page

--- src/Components/Views/PasswodManagerView.tsx
+++ src/Components/Views/PasswodManagerView.tsx
@@ -27,7 +27,7 @@ const PasswordManagerView: React.FC = () => {
                            setOpenedDbName={viewModel.setOpenedDatabaseName}/>
                 <ToastDialog message={viewModel.toastMessage}
                              isVisible={viewModel.toastVisible}
-                             onClose={() => viewModel} />
+                             onClose={() => viewModel}/>
             </RepoContext.Provider>
 
         );
@@ -36,8 +36,10 @@ const PasswordManagerView: React.FC = () => {
             <Suspense fallback={<LoadingScreen/>}>
                 <RepoContext.Provider value={viewModel.repo}>
                     <div className={"password-manager-header"}>
-                        <img src={PWMLogo} className="logo header" alt="Passwortmanager Logo"/>
-                        <h2>LoFi Passwortmanager</h2>
+                        <img src={PWMLogo} style={{cursor: "pointer"}} onClick={() => viewModel.closeLoggedIn()}
+                             className="logo header" alt="Passwortmanager Logo"/>
+                        <h2 onClick={() => viewModel.closeLoggedIn()} style={{cursor: "pointer"}}>LoFi
+                            Passwortmanager</h2>
                         <SettingsView automergeFacade={viewModel.getAutomergeFacade()}
                                       openedDbName={viewModel.openedDatabaseName}
                                       closeDatabase={() => viewModel.closeLoggedIn()}
