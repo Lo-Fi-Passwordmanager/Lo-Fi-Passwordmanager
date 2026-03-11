0834d343f5b4c8e2517cbeb33b05bac0e6260d2d
SwaggiSpaghetti <uueqo@student.kit.edu>
2026-03-11 23:56:07 +0100

    [fix] rename to Datenbank ID

--- src/Components/Views/DialogViews/CreateDatabaseDialog.tsx
+++ src/Components/Views/DialogViews/CreateDatabaseDialog.tsx
@@ -132,13 +132,13 @@ const CreateDatabaseDialog: React.FC<{
                     placeholder={"Name"}
                     autoFocus
                 />
-                <label>Dokument ID</label>
+                <label>Datenbank ID</label>
                 <div className="urlWrapper">
                     <input
                         type="text"
                         value={viewModel.field2}
                         onChange={(e) => viewModel.setField2(e.target.value)}
-                        placeholder={"Dokument ID"}
+                        placeholder={"Datenbank ID"}
                         onKeyDown={(e) => {
                             if (e.key === "Enter") {
                                 viewModel.handleConfirm();
--- src/Components/Views/DialogViews/DatabaseSettingsView.tsx
+++ src/Components/Views/DialogViews/DatabaseSettingsView.tsx
@@ -37,10 +37,10 @@ const DatabaseSettingsView: React.FC<{
                             () => {
                                 void navigator.clipboard.writeText(
                                     (automergeFacade.automergeURL as string).replace("automerge:", ""));
-                                setToast("Dokument ID in die Zwischenablage kopiert")
+                                setToast("Datenbank ID in die Zwischenablage kopiert")
                             }
                         }>
-                        Dokument ID kopieren
+                        Datenbank ID kopieren
                     </button>
                     <ShareQRDialog name={openedDatabaseName!}
                                    url={(automergeFacade.automergeURL as string).replace("automerge:", "")}/>
