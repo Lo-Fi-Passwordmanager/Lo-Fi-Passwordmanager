7d7ed5bfd6473c75bb9caaffe91819c9707b785c
RedCommander735 <40245965+RedCommander735@users.noreply.github.com>
2026-04-03 20:43:35 +0200

    fix: typescript fuckery

--- vite.config.deploy.ts
+++ vite.config.deploy.ts
@@ -2,7 +2,8 @@
 
 // vite.config.ts
 import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react/dist";
+// @ts-ignore
+import react from "@vitejs/plugin-react";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
