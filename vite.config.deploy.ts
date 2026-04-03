26f58dfde1f960762e84a6b4611eea17a04633e8
RedCommander735 <40245965+RedCommander735@users.noreply.github.com>
2026-04-03 20:32:27 +0200

    fix: ts error in deploy config

--- vite.config.deploy.ts
+++ vite.config.deploy.ts
@@ -2,7 +2,7 @@
 
 // vite.config.ts
 import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react/dist";
 import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
